4e27f7e3609f595e70652258e08ee253f0f1c04e
Tomas Petricek <tomas@tomasp.net>
2024-04-24 22:02:42 +0200

    Add Lab 08 materials

--- lab08/json.fsx
+++ lab08/json.fsx
@@ -1,12 +1,3 @@
 #r "nuget: FSharp.Data"
-open FSharp.Data
 
-type Post = JsonProvider<"https://b2c.cpost.cz/services/PostCode/getDataAsJson?cityOrPart=Praha&nameStreet=Malostranske namesti">
-
-let res = Post.GetSamples()
-for no in res do
-  printfn "%s" no.Number.String.Value
-
-
-
-
+// "https://b2c.cpost.cz/services/PostCode/getDataAsJson?cityOrPart=Praha&nameStreet=Malostranske namesti"
